f43d429581153d9cc94c4c2929565bf1d0425253
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-03-09 15:28:45 +0800

    init start

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='app'>
+    
+  </div>
+</template>
+
+<script lang='ts' setup>
+  import { ref } from 'vue'
+  
+</script>
+
+<style scoped>
+  .app {
+    
+  }
+</style>
