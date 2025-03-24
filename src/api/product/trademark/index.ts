b95ca6629b922f83fedb55ffb2218d11dd2d9705
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-03-24 17:47:53 +0800

    对品牌api路径进行提取

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,10 +1,19 @@
 import request from "@/api"
 import type { BrandInfo, BrandId } from './type.ts'
 
+const BRAND_BASE = '/brand'
+enum API{
+    ALL_URL = '/allInfo',
+    GET_URL = '/brandInfo',
+    ADD_URL = '/addInfo',
+    UPDATE_URL = '/updateInfo',
+    DELETE_URL = '/deleteInfo'
+}
+
 // 获取所有品牌
 export function getAllBrands(){
     return request({
-        url: '/allInfo',
+        url: BRAND_BASE + API.ALL_URL,
         method: 'GET'
     })
 }
@@ -12,7 +21,7 @@ export function getAllBrands(){
 // 获取某个品牌
 export function getBrand(brandId : BrandId){
     return request({
-        url: '/brandInfo',
+        url: BRAND_BASE + API.GET_URL,
         method: 'POST',
         data: brandId
     })
@@ -21,7 +30,7 @@ export function getBrand(brandId : BrandId){
 // 添加品牌
 export function addBrand(brandInfo : BrandInfo){
     return request({
-        url: '/addInfo',
+        url: BRAND_BASE + API.ADD_URL,
         method: 'POST',
         data: brandInfo
     })
@@ -30,7 +39,7 @@ export function addBrand(brandInfo : BrandInfo){
 // 更新品牌
 export function updateBrand(brandInfo : BrandInfo){
     return request({
-        url: '/updateInfo',
+        url: BRAND_BASE + API.UPDATE_URL,
         method: 'POST',
         data: brandInfo
     })
@@ -39,7 +48,7 @@ export function updateBrand(brandInfo : BrandInfo){
 // 删除品牌
 export function deleteBrand(brandId : BrandId){
     return request({
-        url: '/addInfo',
+        url: BRAND_BASE + API.DELETE_URL,
         method: 'POST',
         data: brandId
     })
