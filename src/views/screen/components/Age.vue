e1dcfc4c0e4bbe1923f2fd079cf910e9e71e3a77
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-03-31 23:04:32 +0800

    创建年龄饼图

--- src/views/screen/components/Age.vue
+++ src/views/screen/components/Age.vue
@@ -1,16 +1,62 @@
 <template>
     <div class='age'>
-        <h1>年龄展示</h1>
+        <h2>年龄展示</h2>
+        <div class="root" ref="root"></div>
     </div>
 </template>
 
 <script lang='ts' setup>
-    import { ref } from 'vue'
+    import * as echarts from 'echarts'
+    import { ref, onMounted } from 'vue'
+    const root = ref()
+    onMounted(() => {
+        let agePie = echarts.init(root.value)
     
+        agePie.setOption({
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                orient: 'horizontal',
+                left: 'top'
+            },
+            series: [
+                {
+                    type: 'pie',
+                    radius: '50%',
+                    data: [
+                        { value: 100, name: '儿童' },
+                        { value: 735, name: '青年' },
+                        { value: 580, name: '壮年' },
+                        { value: 484, name: '老年' },
+                    ],
+                    emphasis: {
+                        itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ],
+            grid: {
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0
+            }
+        })
+    })
 </script>
 
 <style scoped>
     .age {
-        
+        width: 100%;
+        height: 100%;
+    }
+
+    .root {
+        width: 100%;
+        height: 300px;
     }
 </style>
