0df08a7fd87a3eab3847cc9aa49c7dbf3f55c85b
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-04-07 16:24:25 +0800

    feat: enable PWA support  add an page can be visited offline

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,14 @@ import 'element-plus/dist/index.css'    // msg不显示，因为还要导入css
 import '@/router/permission.ts'
 import 'element-plus/theme-chalk/dark/css-vars.css'// 暗黑模式
 
+if ('serviceWorker' in navigator) {// 注册service worker
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+}  
+
 const app = createApp(App)
 //install(app) // 注册我的全局组件 默认导出随便起个名字就行
 app.use(AllComponents) // 调用 app.use() 时，Vue 会自动寻找你传入对象（或者说是插件）上的 install 方法并执行它。
