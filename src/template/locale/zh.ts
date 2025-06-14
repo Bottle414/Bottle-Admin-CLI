4c5ea1eb19b49f77c37b95eabee9bfae2f31009f
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-06-14 15:23:02 +0800

    feat(template): add i18n support for template

---
+++ src/template/locale/zh.ts
@@ -0,0 +1,23 @@
+const zh = {
+    // 标签名
+    labels: {
+        username: '用户名',
+        role: '用户角色',
+        option: '操作'
+    },
+    // 增删查改
+    curd: {
+        add: '添加',
+        delete: '删除',
+        search: '搜索',
+        edit: '编辑'
+    },
+    placeholder: {
+        username: '请输入用户名'
+    },
+    export: {
+        excel: '导出Excel'
+    }
+}
+
+export default zh
