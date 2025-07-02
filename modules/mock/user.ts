a90452a4b4e9e6a81462c67874a0869032ec5686
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-07-02 21:17:12 +0800

    feat: add test module and useWebStorage testfile

--- modules/mock/user.ts
+++ modules/mock/user.ts
@@ -3,7 +3,7 @@ import type { MockMethod } from 'vite-plugin-mock'
 
 export default [
   {
-    url: '/api/user/register',
+    url: '/loginSlider/gen',
     method: 'post',
     response: ({ body } : { body: { username: string, password: string} }) => {
       return {
@@ -12,41 +12,6 @@ export default [
         data: {
           id: Date.now(),
           username: body.username,
-        }
-      }
-    }
-  },
-  {
-    url: '/api/user/login',
-    method: 'post',
-    response: ({ body }: { body: { username: string; password: string } }) => {
-      if (body.username === 'cat' && body.password === '7777') {
-        return {
-          status: 200,
-          message: 'login success',
-          data: {
-            token: 'mock-token-abc123',
-            userInfo: {
-              id: 1,
-              username: 'cat',
-              role: '老板',
-            },
-          },
-        }
-      }
-      return { status: 500, message: '账号或密码错误' }
-    }
-  },
-  {
-    url: '/api/user/userInfo',
-    method: 'get',
-    response: () => {
-      return {
-        status: 200,
-        data: {
-          id: 9,
-          avatar: 'https://element-plus.org/images/element-plus-logo.svg',
-          routes: ['layout','home','acl','user','role','permission','product','trademark','attr','spu','sku','screen','charts','table']
         }
       }
     }
